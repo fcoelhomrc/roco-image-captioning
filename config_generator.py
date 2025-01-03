10f87c8e5cb3a4613db0bf5c14478c76cd22f44a
Felipe <fcoelho.mrc@gmail.com>
2025-01-03 11:47:18 +0000

    tests - configs with paper parameters for medclip

--- config_generator.py
+++ config_generator.py
@@ -9,8 +9,27 @@ config = {
     "input_size": [224, 224],
     "train": {
         "max_seq_length": 70,
+        "max_epochs": 50,
+        "batch_size": 64,
+        'limit_train_batches': 1.000,
+        'early_stopping_patience': 5,
+        'log_every_n_steps': 4,
+    },
+    "optimizer": {
+        'method': 'adam',
+        'weight_decay': 5e-4,
+        'lr': 1e-5,
+    },
+    "registry": {
+        'project': "roco-debug",
+        'name': "roco-clip",
+        'wandb_root_dir': "outputs/wandb_outputs",
+        'checkpoints_dir': "outputs/model_checkpoints",
     }
 }
 
 with open("config.yaml", "w") as outfile:
     yaml.dump(config, outfile, default_flow_style=False)
+
+from pprint import pprint
+pprint(config)
